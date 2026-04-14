8c779f9ced95e35b0a79a1386b35c4cd6c811db7
harshitshah270307-hub <harshitshah270307@gmail.com>
2026-04-15 01:16:03 +0530

    Update q5.s

--- q5/q5.s
+++ q5/q5.s
@@ -1,101 +1,103 @@
 .section .data
-filename: .asciz "input.txt"
-yesmsg:   .asciz "Yes\n"
-nomsg:    .asciz "No\n"
+file: .asciz "input.txt"
+y:    .asciz "Yes\n"
+n:    .asciz "No\n"
 
 .section .bss
-leftch:   .skip 1
-rightch:  .skip 1
+c1:   .skip 1
+c2:   .skip 1
 
 .section .text
 .globl _start
 
 _start:
-    li      a0, -100
-    la      a1, filename
-    li      a2, 0
-    li      a3, 0
-    li      a7, 56
+    li    a0, -100
+    la    a1, file
+    li    a2, 0
+    li    a3, 0
+    li    a7, 56
     ecall
-    mv      s0, a0
+    mv    s0, a0
 
-    bltz    s0, print_no
+    bltz  s0, no_out
 
-    mv      a0, s0
-    li      a1, 0
-    li      a2, 2
-    li      a7, 62
+    mv    a0, s0
+    li    a1, 0
+    li    a2, 2
+    li    a7, 62
     ecall
-    mv      s1, a0
+    mv    s1, a0
 
-    li      s2, 0
-    addi    s3, s1, -1
+    li    s2, 0
+    addi  s3, s1, -1
 
-loop:
-    bge     s2, s3, print_yes
+l1:
+    bge   s2, s3, yes_out
 
-    mv      a0, s0
-    mv      a1, s2
-    li      a2, 0
-    li      a7, 62
-    ecall
-
-    mv      a0, s0
-    la      a1, leftch
-    li      a2, 1
-    li      a7, 63
+    mv    a0, s0
+    mv    a1, s2
+    li    a2, 0
+    li    a7, 62
     ecall
 
-    mv      a0, s0
-    mv      a1, s3
-    li      a2, 0
-    li      a7, 62
+    mv    a0, s0
+    la    a1, c1
+    li    a2, 1
+    li    a7, 63
     ecall
 
-    mv      a0, s0
-    la      a1, rightch
-    li      a2, 1
-    li      a7, 63
+    mv    a0, s0
+    mv    a1, s3
+    li    a2, 0
+    li    a7, 62
     ecall
 
-    la      t0, leftch
-    lb      t1, 0(t0)
-    la      t0, rightch
-    lb      t2, 0(t0)
-
-    bne     t1, t2, print_no
-
-    addi    s2, s2, 1
-    addi    s3, s3, -1
-    j       loop
-
-print_yes:
-    mv      a0, s0
-    li      a7, 57
+    mv    a0, s0
+    la    a1, c2
+    li    a2, 1
+    li    a7, 63
     ecall
 
-    li      a0, 1
-    la      a1, yesmsg
-    li      a2, 4
-    li      a7, 64
-    ecall
-    j       exit_program
+    la    t0, c1
+    lb    t1, 0(t0)
+    la    t0, c2
+    lb    t2, 0(t0)
 
-print_no:
-    bltz    s0, skip_close_no
-    mv      a0, s0
-    li      a7, 57
-    ecall
+    beq   t1, t2, l2
+    j     no_out
 
-skip_close_no:
-    li      a0, 1
-    la      a1, nomsg
-    li      a2, 3
-    li      a7, 64
+l2:
+    addi  s2, s2, 1
+    addi  s3, s3, -1
+    j     l1
+
+yes_out:
+    mv    a0, s0
+    li    a7, 57
     ecall
 
-exit_program:
-    li      a0, 0
-    li      a7, 93
+    li    a0, 1
+    la    a1, y
+    li    a2, 4
+    li    a7, 64
+    ecall
+    j     done
+
+no_out:
+    bltz  s0, l3
+    mv    a0, s0
+    li    a7, 57
+    ecall
+
+l3:
+    li    a0, 1
+    la    a1, n
+    li    a2, 3
+    li    a7, 64
+    ecall
+
+done:
+    li    a0, 0
+    li    a7, 93
     ecall
     
