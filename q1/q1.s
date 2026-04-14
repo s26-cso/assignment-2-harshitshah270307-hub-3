a46f80d8a3318ccb80ee2d323e9fe5780b15e11e
harshitshah270307-hub <harshitshah270307@gmail.com>
2026-04-15 00:23:01 +0530

    Update q1.s

--- q1/q1.s
+++ q1/q1.s
@@ -1,119 +1,106 @@
-.equ NODE_SIZE,  24
-.equ VAL_OFF,    0
-.equ LEFT_OFF,   8
-.equ RIGHT_OFF,  16
+.equ NODE_SIZE,   24
+.equ VAL_OFF,     0
+.equ LEFT_OFF,    8
+.equ RIGHT_OFF,   16
 
 .text
-
 .globl make_node
 make_node:
-    addi  sp, sp, -16
-    sd    ra, 8(sp)
-    sd    s0, 0(sp)
+    addi sp, sp, -16
+    sd   ra, 8(sp)
+    sd   s1, 0(sp)
 
-    mv    s0, a0
+    mv   s1, a0          
+    li   a0, NODE_SIZE
+    call malloc
 
-    li    a0, NODE_SIZE
-    call  malloc
-
-    sw    s0, VAL_OFF(a0)
-    sd    zero, LEFT_OFF(a0)
-    sd    zero, RIGHT_OFF(a0)
-
-    ld    ra, 8(sp)
-    ld    s0, 0(sp)
-    addi  sp, sp, 16
+    sw   s1, VAL_OFF(a0)
+    sd   zero, LEFT_OFF(a0)
+    sd   zero, RIGHT_OFF(a0)
+    ld   s1, 0(sp)
+    ld   ra, 8(sp)
+    addi sp, sp, 16
     ret
 
 .globl insert
 insert:
-    addi  sp, sp, -32
-    sd    ra,  24(sp)
-    sd    s0,  16(sp)
-    sd    s1,   8(sp)
-    sd    s2,   0(sp)
+    addi sp, sp, -32
+    sd   ra, 24(sp)
+    sd   s0, 16(sp)
+    sd   s1, 8(sp)
+    sd   s2, 0(sp)
+    mv   s0, a0      
+    mv   s1, a1      
+    beqz s0, make_new
+    lw   s2, VAL_OFF(s0)
+    blt  s1, s2, go_left
+    beq  s1, s2, same_val
 
-    mv    s0, a0
-    mv    s1, a1
+go_right:
+    ld   a0, RIGHT_OFF(s0)
+    mv   a1, s1
+    call insert
+    sd   a0, RIGHT_OFF(s0)
+    mv   a0, s0
+    j    done_insert
 
-    bnez  s0, insert_non_null
-    mv    a0, s1
-    call  make_node
-    j     insert_done
+go_left:
+    ld   a0, LEFT_OFF(s0)
+    mv   a1, s1
+    call insert
+    sd   a0, LEFT_OFF(s0)
+    mv   a0, s0
+    j    done_insert
 
-insert_non_null:
-    lw    s2, VAL_OFF(s0)
+same_val:
+    mv   a0, s0
+    j    done_insert
+make_new:
+    mv   a0, s1
+    call make_node
 
-    bge   s1, s2, insert_right_check
-
-    ld    a0, LEFT_OFF(s0)
-    mv    a1, s1
-    call  insert
-    sd    a0, LEFT_OFF(s0)
-    mv    a0, s0
-    j     insert_done
-
-insert_right_check:
-    beq   s1, s2, insert_equal
-
-    ld    a0, RIGHT_OFF(s0)
-    mv    a1, s1
-    call  insert
-    sd    a0, RIGHT_OFF(s0)
-    mv    a0, s0
-    j     insert_done
-
-insert_equal:
-    mv    a0, s0
-
-insert_done:
-    ld    ra,  24(sp)
-    ld    s0,  16(sp)
-    ld    s1,   8(sp)
-    ld    s2,   0(sp)
-    addi  sp, sp, 32
+done_insert:
+    ld   s2, 0(sp)
+    ld   s1, 8(sp)
+    ld   s0, 16(sp)
+    ld   ra, 24(sp)
+    addi sp, sp, 32
     ret
 
 .globl get
 get:
-get_loop:
-    beqz  a0, get_done
+loop_get:
+    beqz a0, end_get
+    lw   t0, VAL_OFF(a0)
+    beq  a1, t0, end_get
+    blt  a1, t0, left_get
+    ld   a0, RIGHT_OFF(a0)
+    j    loop_get
 
-    lw    t0, VAL_OFF(a0)
-    beq   a1, t0, get_done
-
-    blt   a1, t0, get_go_left
-    ld    a0, RIGHT_OFF(a0)
-    j     get_loop
-get_go_left:
-    ld    a0, LEFT_OFF(a0)
-    j     get_loop
-
-get_done:
+left_get:
+    ld   a0, LEFT_OFF(a0)
+    j    loop_get
+end_get:
     ret
 
 .globl getAtMost
 getAtMost:
-    li    t0, -1
-    mv    t1, a1
-    mv    t2, a0
+    li   t0, -1 
+    mv   t1, a0      
+    mv   t2, a1       
 
-getAtMost_loop:
-    beqz  t1, getAtMost_done
+loop_am:
+    beqz t2, end_am
+    lw   t3, VAL_OFF(t2)
+    blt  t1, t3, go_left_am
+    mv   t0, t3
+    ld   t2, RIGHT_OFF(t2)
+    j    loop_am
 
-    lw    t3, VAL_OFF(t1)
+go_left_am:
+    ld   t2, LEFT_OFF(t2)
+    j    loop_am
 
-    bgt   t3, t2, getAtMost_go_left
-
-    mv    t0, t3
-    ld    t1, RIGHT_OFF(t1)
-    j     getAtMost_loop
-
-getAtMost_go_left:
-    ld    t1, LEFT_OFF(t1)
-    j     getAtMost_loop
-
-getAtMost_done:
-    mv    a0, t0
+end_am:
+    mv   a0, t0
     ret
-    
